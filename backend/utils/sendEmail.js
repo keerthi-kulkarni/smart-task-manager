e5d6d0521e51bc7d88d376c262f22c19dcd1faf7
keerthi-kulkarni <keerthikulkarni22@gmail.com>
2026-07-06 08:58:06 +0530

    Add SMTP verify

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,20 +1,27 @@
 import nodemailer from "nodemailer";
 
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
+
+transporter.verify((error, success) => {
+  if (error) {
+    console.error("SMTP Error:", error);
+  } else {
+    console.log("SMTP Ready");
+  }
+});
+
 const sendEmail = async ({ to, subject, html }) => {
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS
-    }
-  });
-
   await transporter.sendMail({
     from: `"Smart Task Manager" <${process.env.EMAIL_USER}>`,
     to,
     subject,
-    html
+    html,
   });
 };
 
